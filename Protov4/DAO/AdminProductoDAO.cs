f5aa35502a0e715ce534dadb1777de1ce95cfb92
IxVerdi1518 <estebanfe400@gmail.com>
2023-08-13 13:11:11 -0500

    Agregar el dao de admin

---
+++ Protov4/DAO/AdminProductoDAO.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+using Protov4.DTO;
+
+namespace Protov4.DAO
+{
+    public class AdminProductoDAO
+    {
+        private readonly IMongoCollection<ProductoDTO> prod;
+        public AdminProductoDAO(IConfiguration configuration)
+        {
+            var mongo = new DBMongo(configuration);
+            prod = mongo.GetDatabase().GetCollection<ProductoDTO>("Productos");
+        }
+
+    }
+}
