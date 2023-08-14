551182d3621710b2ed29b513e28dbd67e35206dc
IxVerdi1518 <estebanfe400@gmail.com>
2023-08-13 20:02:32 -0500

    a

--- Protov4/DAO/ProductosAdminDAO.cs
+++ Protov4/DAO/ProductosAdminDAO.cs
@@ -21,11 +21,11 @@ namespace Protov4.DAO
                 productotoreDatabaseSettings.Value.MikuCollection);
         }
 
-        public async Task<List<Book>> GetAsync() =>
-            await _booksCollection.Find(_ => true).ToListAsync();
+        public async Task<List<ProductoDTO>> GetAsync() =>
+            await _productoCollection.Find(_ => true).ToListAsync();
 
-        public async Task<Book?> GetAsync(string id) =>
-            await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        public async Task<ProductoDTO?> GetAsync(string id) =>
+            await _productoCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
         public async Task CreateAsync(Book newBook) =>
             await _booksCollection.InsertOneAsync(newBook);
@@ -36,5 +36,4 @@ namespace Protov4.DAO
         public async Task RemoveAsync(string id) =>
             await _booksCollection.DeleteOneAsync(x => x.Id == id);
     }
-}
 }
